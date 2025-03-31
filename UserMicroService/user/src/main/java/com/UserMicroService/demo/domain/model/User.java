1207c55bce290d876f02d470a3aa5998378d4265
arthurritzel <ritzelarthur@gmail.com>
2025-03-31 20:15:03 -0300

    object value user

--- UserMicroService/user/src/main/java/com/UserMicroService/demo/domain/model/User.java
+++ UserMicroService/user/src/main/java/com/UserMicroService/demo/domain/model/User.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.util.regex.Pattern;
 
+
 @Entity
 @Data
 @Table(name = "users")
@@ -18,34 +19,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String email;
-    private String senha;
-    private String telefone;
-    private String rua;
-    private String numero;
-    private String cidade;
-    private String cep;
-    private String cpf;
+
+    @Embedded
+    private Email email;
+
+    @Embedded
+    private Senha senha;
+
+    @Embedded
+    private Telefone telefone;
+
+    @Embedded
+    private Endereco endereco;
+
+    @Embedded
+    private Cpf cpf;
+
     private LocalDate dataNascimento;
     private LocalDate dataCadastro;
-
-
-    public void setNome(String nome) {
-        if (nome == null || nome.trim().length() < 3) {
-            throw new IllegalArgumentException("O nome deve ter pelo menos 3 caracteres.");
-        }
-        this.nome = nome;
-    }
-
-    public void setCpf(String cpf) {
-        if (!isValidCpf(cpf)) {
-            throw new IllegalArgumentException("CPF inválido.");
-        }
-        this.cpf = cpf;
-    }
-
-    private boolean isValidCpf(String cpf) {
-        String regex = "\\d{11}";
-        return Pattern.matches(regex, cpf);
-    }
 }
